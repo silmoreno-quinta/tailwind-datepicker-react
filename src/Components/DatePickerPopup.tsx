0d47407d8f587dee1bb9cfa35f1407ebcc614e91
Silvestre Moreno <silvestre@quinta.tech>
2025-02-19 12:44:13 -0600

    * refactor(DatePickerPopup.tsx): * - Refactor code formatting and indentation for better readability. * - Change variable name `setShow` to `setDatePickerShow` for clarity. * - Add missing `e.stopPropagation()` in onClick event handler. * - Fix typo in `e.stopPropagation()`. * * feat(DatePickerPopup.tsx): * - Add click event to close the date picker when clicking outside the popup.

--- src/Components/DatePickerPopup.tsx
+++ src/Components/DatePickerPopup.tsx
@@ -9,39 +9,47 @@ import Months from "./Views/Months"
 import Years from "./Views/Years"
 
 const DatePickerPopup = forwardRef<HTMLDivElement>((_props, ref: ForwardedRef<HTMLDivElement>) => {
-	const { selectedMonth, selectedYear, view, options } = useContext(DatePickerContext)
-	
-	const language = options?.language ? options?.language : "en";
-    // @ts-ignore
-	const locale = new Intl.Locale(language);
-	
-	const weekStart = (locale?.weekInfo?.firstDay || 1);
+	const { selectedMonth, selectedYear, view, options, setShow } = useContext(DatePickerContext)
+
+	const language = options?.language ? options?.language : "en"
+	// @ts-ignore
+	const locale = new Intl.Locale(language)
+
+	const weekStart = locale?.weekInfo?.firstDay || 1
 	const firstOfMonth = firstDateOfMonth(selectedYear, selectedMonth, 1)
-	const start = dayOfTheWeekOf(firstOfMonth, weekStart, weekStart);
-	
+	const start = dayOfTheWeekOf(firstOfMonth, weekStart, weekStart)
+
 	return (
-		<div ref={ref} className={twMerge("absolute z-50 block pt-2 top-10", options?.datepickerClassNames)}>
-			<div className={twMerge("inline-block p-4 bg-white rounded-lg shadow-lg dark:bg-gray-700", options?.theme?.background)}>
-				<div>
-					{options?.title && <div className={twMerge("px-2 py-3 font-semibold text-center text-gray-900 dark:text-white", options?.theme?.text)}>{options?.title}</div>}
-					<div className="flex justify-between mb-2">
-						<ButtonPrevMonth />
-						<ButtonSelectMonth />
-						<ButtonNextMonth />
+		<div
+			className="fixed inset-0 flex items-center justify-center z-50"
+			onClick={(e) => {
+				e.stopPropagination()
+				setShow(false)
+			}}
+		>
+			<div ref={ref} className={twMerge("absolute block pt-2 top-10", options?.datepickerClassNames)}>
+				<div className={twMerge("inline-block p-4 bg-white rounded-lg shadow-lg dark:bg-gray-700", options?.theme?.background)}>
+					<div>
+						{options?.title && <div className={twMerge("px-2 py-3 font-semibold text-center text-gray-900 dark:text-white", options?.theme?.text)}>{options?.title}</div>}
+						<div className="flex justify-between mb-2">
+							<ButtonPrevMonth />
+							<ButtonSelectMonth />
+							<ButtonNextMonth />
+						</div>
 					</div>
-				</div>
-				<div className="p-1">
-					{view === "days" && <Days start={start} />}
-					{view === "months" && <Months />}
-					{view === "years" && <Years />}
-					{view === "decades" && <Decades />}
-				</div>
-				{(options?.todayBtn || options?.clearBtn) && (
-					<div className="flex mt-2 space-x-2">
-						{options?.todayBtn && <ButtonToday />}
-						{options?.clearBtn && <ButtonClear />}
+					<div className="p-1">
+						{view === "days" && <Days start={start} />}
+						{view === "months" && <Months />}
+						{view === "years" && <Years />}
+						{view === "decades" && <Decades />}
 					</div>
-				)}
+					{(options?.todayBtn || options?.clearBtn) && (
+						<div className="flex mt-2 space-x-2">
+							{options?.todayBtn && <ButtonToday />}
+							{options?.clearBtn && <ButtonClear />}
+						</div>
+					)}
+				</div>
 			</div>
 		</div>
 	)
